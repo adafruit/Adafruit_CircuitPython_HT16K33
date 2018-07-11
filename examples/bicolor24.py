895c2dd12df0add2be5a738696efa332b14a9458
caternuson <methuzla@speakeasy.net>
2018-07-11 08:41:48 -0700

    add comments to example

--- examples/bicolor24.py
+++ examples/bicolor24.py
@@ -1,20 +1,34 @@
+# Basic example of using the Bi-color 24 segment bargraph display.
+# This example and library is meant to work with Adafruit CircuitPython API.
+# Author: Carter Nelson
+# License: Public Domain
+
 import time
+
+# Import board related modules
 import board
 import busio
+
+# Import the Bicolor24 driver from the HT16K33 module
 from adafruit_ht16k33.bargraph import Bicolor24
 
+# Create the I2C interface
 i2c = busio.I2C(board.SCL, board.SDA)
 
+# Create the LED bargraph class.
 bar = Bicolor24(i2c)
 
+# Set individual segments of bargraph
 bar[0] = bar.LED_RED
 bar[1] = bar.LED_GREEN
 bar[2] = bar.LED_YELLOW
 
 time.sleep(2)
 
+# Turn them all off
 bar.fill(bar.LED_OFF)
 
+# Turn them on in a loop
 for i in range(24):
     bar[i] = bar.LED_RED
     time.sleep(0.1)
@@ -22,4 +36,5 @@ for i in range(24):
 
 time.sleep(1)
 
+# Fill the entrire bargraph
 bar.fill(bar.LED_GREEN)
