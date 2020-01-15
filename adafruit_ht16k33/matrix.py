0f4ede8f11921a4c00e062bf64aba60913525826
Melissa LeBlanc-Williams <melissa@adafruit.com>
2020-01-15 10:22:00 -0800

    Added Shift functions for matrix

--- adafruit_ht16k33/matrix.py
+++ adafruit_ht16k33/matrix.py
@@ -33,6 +33,9 @@ __repo__ = "https://github.com/adafruit/Adafruit_CircuitPython_HT16K33.git"
 
 class Matrix8x8(HT16K33):
     """A single matrix."""
+    columns = 8
+    rows = 8
+
     def pixel(self, x, y, color=None):
         """Get or set the color of a given pixel."""
         if not 0 <= x <= 7:
@@ -50,8 +53,67 @@ class Matrix8x8(HT16K33):
         x, y = key
         self.pixel(x, y, value)
 
+    def shift_right(self, rotate=False):
+        """
+        Shift all pixels right
+
+        :param rotate: (Optional) Rotate the shifted pixels to the left side (default=False)
+        """
+        for y in range(0, self.rows):
+            last_pixel = self[self.columns - 1, y] if rotate else 0
+            for x in range(self.columns - 1, 0, -1):
+                self[x, y] = self[x - 1, y]
+            self[0, y] = last_pixel
+        if self._auto_write:
+            self.show()
+
+    def shift_left(self, rotate=False):
+        """
+        Shift all pixels left
+
+        :param rotate: (Optional) Rotate the shifted pixels to the right side (default=False)
+        """
+        for y in range(0, self.rows):
+            last_pixel = self[0, y] if rotate else 0
+            for x in range(0, self.columns - 1):
+                self[x, y] = self[x + 1, y]
+            self[self.columns - 1, y] = last_pixel
+        if self._auto_write:
+            self.show()
+
+    def shift_up(self, rotate=False):
+        """
+        Shift all pixels up
+
+        :param rotate: (Optional) Rotate the shifted pixels to bottom (default=False)
+        """
+        for x in range(0, self.columns):
+            last_pixel = self[x, self.rows - 1] if rotate else 0
+            for y in range(self.rows - 1, 0, -1):
+                self[x, y] = self[x, y - 1]
+            self[x, 0] = last_pixel
+        if self._auto_write:
+            self.show()
+
+    def shift_down(self, rotate=False):
+        """
+        Shift all pixels down
+
+        :param rotate: (Optional) Rotate the shifted pixels to top (default=False)
+        """
+        for x in range(0, self.columns):
+            last_pixel = self[x, 0] if rotate else 0
+            for y in range(0, self.rows - 1):
+                self[x, y] = self[x, y + 1]
+            self[x, self.rows - 1] = last_pixel
+        if self._auto_write:
+            self.show()
+
 class Matrix16x8(Matrix8x8):
     """The matrix wing."""
+    columns = 16
+    rows = 8
+
     def pixel(self, x, y, color=None):
         """Get or set the color of a given pixel."""
         if not 0 <= x <= 15:
@@ -63,7 +125,7 @@ class Matrix16x8(Matrix8x8):
             y += 8
         return super()._pixel(y, x, color)
 
-class MatrixBackpack16x8(Matrix8x8):
+class MatrixBackpack16x8(Matrix16x8):
     """A double matrix backpack."""
     def pixel(self, x, y, color=None):
         """Get or set the color of a given pixel."""
--- examples/ht16k33_matrix_simpletest.py
+++ examples/ht16k33_matrix_simpletest.py
@@ -4,6 +4,7 @@
 # License: Public Domain
 
 # Import all board pins.
+import time
 import board
 import busio
 
@@ -29,9 +30,47 @@ matrix = matrix.Matrix16x8(i2c)
 # Clear the matrix.
 matrix.fill(0)
 
-# Set a pixel in the origin 0,0 position.
+# Set a pixel in the origin 0, 0 position.
 matrix[0, 0] = 1
 # Set a pixel in the middle 8, 4 position.
 matrix[8, 4] = 1
 # Set a pixel in the opposite 15, 7 position.
 matrix[15, 7] = 1
+
+time.sleep(2)
+
+# Draw a Smiley Face
+for row in range(2, 6):
+    matrix[row, 0] = 1
+    matrix[row, 7] = 1
+
+for column in range(2, 6):
+    matrix[0, column] = 1
+    matrix[7, column] = 1
+
+matrix[1, 1] = 1
+matrix[1, 6] = 1
+matrix[6, 1] = 1
+matrix[6, 6] = 1
+matrix[2, 5] = 1
+matrix[5, 5] = 1
+matrix[2, 3] = 1
+matrix[5, 3] = 1
+matrix[3, 2] = 1
+matrix[4, 2] = 1
+
+# Move the Smiley Face Around
+while True:
+    for frame in range(0, 8):
+        matrix.shift_right(True)
+        time.sleep(0.05)
+    for frame in range(0, 8):
+        matrix.shift_down(True)
+        time.sleep(0.05)
+    for frame in range(0, 8):
+        matrix.shift_left(True)
+        time.sleep(0.05)
+    for frame in range(0, 8):
+        matrix.shift_up(True)
+        time.sleep(0.05)
+
