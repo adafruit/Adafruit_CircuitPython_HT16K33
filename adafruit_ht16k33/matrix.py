3b475777a12bf5f9cb9b4a7ff8851f9f69308d32
Steve Kay <Steve@PyxisRC.com>
2021-02-10 23:25:31 -0800

    Disable display refresh during update in Matrix8x8
    In Matrix8x8.image(), store current auto_write state
    in temp variable and set auto_write to False before
    looping to update all pixels in the matrix.  Once done
    updating, restore auto_write state to previous setting.

--- adafruit_ht16k33/matrix.py
+++ adafruit_ht16k33/matrix.py
@@ -124,10 +124,13 @@ class Matrix8x8(HT16K33):
             )
         # Grab all the pixels from the image, faster than getpixel.
         pixels = img.convert("1").load()
+        auto_write = self.auto_write
+        self._auto_write = False
         # Iterate through the pixels
         for x in range(self.columns):  # yes this double loop is slow,
             for y in range(self.rows):  #  but these displays are small!
                 self.pixel(x, y, pixels[(x, y)])
+        self._auto_write = auto_write
         if self._auto_write:
             self.show()
 
